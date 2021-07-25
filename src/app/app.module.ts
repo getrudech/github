0a2cc3781d4207f446e05af114c3f6d40df69dd4
getty <gcherono15@gmail.com>
2021-07-25 22:06:13 +0300

    updated app.component.html to give it a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RepoComponent } from './repo/repo.component';
+import { RepoComponent } from './repos/repo.component';
 
 @NgModule({
   declarations: [
